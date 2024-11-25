a489da8edf9029b4abe8e614261ba6384744827f
Martin <martinbog19@gmail.com>
2024-11-25 10:06:49 -0500

    update commit

--- schedule_today_games.py
+++ schedule_today_games.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import datetime, timedelta, timezone
 from zoneinfo import ZoneInfo
+import pytz
+import json
 
 
 def datetime_to_cron(dt):
@@ -13,7 +15,7 @@ def datetime_to_cron(dt):
 season = 2025
 today = datetime.now(ZoneInfo('America/New_York'))
 
-
+api_key = 'a7bfde5bb651ac64e61f99c67631ef47'
 
 url = f'https://www.basketball-reference.com/leagues/NBA_{season}_games-{today.strftime("%B").lower()}.html'
 page = requests.get(url)
@@ -29,8 +31,29 @@ games = games[pd.to_datetime(games['Date']) == pd.to_datetime(today.date())].res
 games['Time'] = (games['Date'].astype(str) + ' ' +  games['Time']).apply(lambda x: datetime.strptime(x.upper() + 'M', "%Y-%m-%d %I:%M%p"))
 games['game_id'] = games['Date'].apply(lambda x: datetime.strftime(x, "%Y%m%d")) + '0' + games['Home']
 games = games[['game_id', 'Date', 'Time', 'Home', 'Away']]
+
+tmrw_utc = (today + timedelta(days = 1)).astimezone(pytz.utc)
+
+events_response = requests.get(f'https://api.the-odds-api.com/v4/sports/basketball_nba/events',
+                               params = {'apiKey': api_key,
+                                         'commenceTimeTo': tmrw_utc.strftime('%Y-%m-%dT%H:%M:%SZ')})
+
+
+events = pd.DataFrame(events_response.json()).rename(columns = {'id': 'event_id'})
+events['commence_time'] = pd.to_datetime(events['commence_time'])
+
+
+with open('utils/odds_tm_map.json', 'r') as file:
+    odds_tm_map = json.load(file)
+
+
+events['game_id'] = today.strftime("%Y%m%d") + '0' + events['home_team'].map(odds_tm_map)
+
+
+games = pd.merge(games, events[['game_id', 'event_id']], on = 'game_id')
+
 games['insert_timestamp_utc'] = datetime.now(timezone.utc)
-games.to_csv('data/games.csv', index = None, mode = 'a')
+games.to_csv('data/games.csv', index = None, mode = 'w')
 
 
 execute_crons = [datetime_to_cron(t - timedelta(minutes = 30)) for t in games['Time'].unique()]
