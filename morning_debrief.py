32fbf5098e09d69f5fdf395f39f35b5a1daa444a
Martin <martinbog19@gmail.com>
2024-12-19 07:31:27 -0500

    refactor for days with 0 games

--- morning_debrief.py
+++ morning_debrief.py
@@ -51,55 +51,55 @@ for i, gameId in enumerate(game_ids) :
     roster = get_roster(gameId)
     roster.to_csv('data/rosters.nosync/rosters_2025.csv', index = None, header = None, mode = 'a')
 
-first_basket_df = pd.concat(dfs).set_index('game_id')
-first_basket_df['correct_pred'] = (first_basket_df['first_basket'] == first_basket_df['first_basket_pred'])
-first_basket_df['correct_rand'] = (first_basket_df['first_basket'] == first_basket_df['first_basket_rand'])
-first_basket_df = first_basket_df[['Date', 'Home', 'Away', 'first_basket', 'first_basket_tm', 'first_basket_pred', 'correct_pred', 'first_basket_rand', 'correct_rand']]
+if len(dfs) > 0 :
 
-# Map id's to player names
-player_metadata = pd.read_csv('data/player_metadata.csv')
-playerId_map = dict(zip(player_metadata['player_id'], player_metadata['name']))
-for player_col in ['first_basket', 'first_basket_pred', 'first_basket_rand'] :
-    first_basket_df[player_col] = first_basket_df[player_col].map(playerId_map)
+    first_basket_df = pd.concat(dfs).set_index('game_id')
+    first_basket_df['correct_pred'] = (first_basket_df['first_basket'] == first_basket_df['first_basket_pred'])
+    first_basket_df['correct_rand'] = (first_basket_df['first_basket'] == first_basket_df['first_basket_rand'])
+    first_basket_df = first_basket_df[['Date', 'Home', 'Away', 'first_basket', 'first_basket_tm', 'first_basket_pred', 'correct_pred', 'first_basket_rand', 'correct_rand']]
 
-acc_pred = first_basket_df['correct_pred'].mean()
-acc_rand = first_basket_df['correct_rand'].mean()
+    # Map id's to player names
+    player_metadata = pd.read_csv('data/player_metadata.csv')
+    playerId_map = dict(zip(player_metadata['player_id'], player_metadata['name']))
+    for player_col in ['first_basket', 'first_basket_pred', 'first_basket_rand'] :
+        first_basket_df[player_col] = first_basket_df[player_col].map(playerId_map)
 
-print(f'\nAccuracy predicted : {round(100 * acc_pred, 1)}%  [{first_basket_df["correct_pred"].sum()}/{first_basket_df.shape[0]}]\n')
-print(f'\nAccuracy random    : {round(100 * acc_rand, 1)}%  [{first_basket_df["correct_rand"].sum()}/{first_basket_df.shape[0]}]\n\n')
-print(first_basket_df, '\n\n\n')
+    acc_pred = first_basket_df['correct_pred'].mean()
+    acc_rand = first_basket_df['correct_rand'].mean()
 
-date = yst.strftime("%d %b %Y")
-synopsis = f'[LeFirstBasket | {date}] Random model: {round(100 * acc_rand, 1)}% | Predicted model: {round(100 * acc_pred, 1)}%'
+    print(f'\nAccuracy predicted : {round(100 * acc_pred, 1)}%  [{first_basket_df["correct_pred"].sum()}/{first_basket_df.shape[0]}]\n')
+    print(f'\nAccuracy random    : {round(100 * acc_rand, 1)}%  [{first_basket_df["correct_rand"].sum()}/{first_basket_df.shape[0]}]\n\n')
+    print(first_basket_df, '\n\n\n')
 
-send_email(first_basket_df,
-           receivers = ['martinbog19@gmail.com', 'lucas.leforestier@gmail.com'],
-           subject = synopsis)
+    date = yst.strftime("%d %b %Y")
+    synopsis = f'[LeFirstBasket | {date}] Random model: {round(100 * acc_rand, 1)}% | Predicted model: {round(100 * acc_pred, 1)}%'
+
+    send_email(first_basket_df,
+            receivers = ['martinbog19@gmail.com', 'lucas.leforestier@gmail.com'],
+            subject = synopsis)
 
 
 
+    # Send ML preds
+    data, features = feature_engineering()
 
+    games = data.copy()[data['game_id'].isin(game_ids)].reset_index(drop = True)
 
-# Send ML preds
-data, features = feature_engineering()
+    X = games[features].to_numpy()
 
-games = data.copy()[data['game_id'].isin(game_ids)].reset_index(drop = True)
+    # Load the model from the .pkl file
+    with open('models/model_rf.pkl', "rb") as f:
+        model = pickle.load(f)
 
-X = games[features].to_numpy()
+    y_pred = model.predict_proba(X)[:, -1]
 
-# Load the model from the .pkl file
-with open('models/model_rf.pkl', "rb") as f:
-    model = pickle.load(f)
+    games = games[['game_id', 'player_id', 'Player']]
+    games['Pred. prob (%)'] = y_pred
+    games['Pred. prob (%)'] = games['Pred. prob (%)'] / games.groupby('game_id')['Pred. prob (%)'].transform('sum')
+    games['Pred. odds'] = games['Pred. prob (%)'].apply(lambda x: round(1/x, 1))
+    games['Pred. prob (%)'] = games['Pred. prob (%)'].apply(lambda x: round(x * 100, 1))
+    games = games.sort_values(['game_id', 'Pred. odds']).reset_index(drop = True)
 
-y_pred = model.predict_proba(X)[:, -1]
-
-games = games[['game_id', 'player_id', 'Player']]
-games['Pred. prob (%)'] = y_pred
-games['Pred. prob (%)'] = games['Pred. prob (%)'] / games.groupby('game_id')['Pred. prob (%)'].transform('sum')
-games['Pred. odds'] = games['Pred. prob (%)'].apply(lambda x: round(1/x, 1))
-games['Pred. prob (%)'] = games['Pred. prob (%)'].apply(lambda x: round(x * 100, 1))
-games = games.sort_values(['game_id', 'Pred. odds']).reset_index(drop = True)
-
-send_email(games,
-           receivers = ['martinbog19@gmail.com', 'lucas.leforestier@gmail.com'],
-           subject = '[LeFirstBasket] MACHINE LEARNING PREDICTIONS!')
+    send_email(games,
+            receivers = ['martinbog19@gmail.com', 'lucas.leforestier@gmail.com'],
+            subject = '[LeFirstBasket] MACHINE LEARNING PREDICTIONS!')
